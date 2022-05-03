100cdecac6483325feb2ec7d622ea5b3384d82d9
XATTAB <XATTAB.rus@gmail.com>
2022-05-03 21:53:00 +0500

    Add files via upload

--- test-content.js
+++ test-content.js
@@ -1,23 +1,9 @@
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         if (request.message == "clicked_browser_action") {
-//             find_person(request.oms_number, request.birthday);
-
-//             switch (request.service_id) {
-//                 case 1:
-//                     executeFuncAfterLoadDom(0, 10, function() { $('a:contains("Вызов врача на дом")').parent().trigger('click'); });
-//                 case 2:
-//                     executeFuncAfterLoadDom(0, 10, function() { $('a:contains("Запись к врачу в МО по прикреплению")').parent().trigger('click'); });
-//                 case 3:
-//                     executeFuncAfterLoadDom(0, 10, function() { $('a:contains("Запись к врачу в другие МО")').parent().trigger('click'); });
-//             };
-
-//             create_line('Услуга', request.service, null, 'bot_service_bt_set');
-//             create_line('Номер телефона', request.full_phonenumber, 'bot_phone', 'bot_phone_bt_set');
-//             create_line('ОМС', request.oms_number);
-//             create_line('День рождения', request.birthday);
-//         }
-//     }
-// );
-
 alert('test');
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message == "clicked_browser_action") {
+            alert(request.oms_number);
+        }
+    }
+);
